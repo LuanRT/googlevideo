24ac023ca75db3c561235b8e81b7999c0f41a1d1
Luan <luan.lrt4@gmail.com>
2025-07-18 10:54:44 -0300

    chore: Remove browser build script
    Not really needed.

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './core/CompositeBuffer.js';
-export * from './core/SabrStream.js';
-export * from './core/SabrStreamingAdapter.js';
-export * from './core/SabrUmpProcessor.js';
-export * from './core/UmpReader.js';
-export * from './core/UmpWriter.js';
-export * from './utils/index.js';
-export * as Protos from './exports/protos.js';
